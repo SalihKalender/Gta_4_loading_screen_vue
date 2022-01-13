54ad5e04f37fe411465f50134e1490455c75a692
SalihKalender <m.salihk72@gmail.com>
2022-01-13 18:59:09 +0300

    bg'lerin efekti eklendi, devam et

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,61 @@
   import rockstar_games_logo from './assets/img/rockstar_games_logo.png'
   import rockstar_north_logo from './assets/img/rockstar_north_logo.png'
   import gta_4_logo from './assets/img/gta_4_logo.png'
+  import loading_audio_url from './assets/audio/gta_4_loading_screen_theme.mp3'
+
+  import bg_1 from './assets/img/bg_1.jpg'
+  import bg_2 from './assets/img/bg_2.jpg'
+  import bg_3 from './assets/img/bg_3.jpg'
+  import person_1 from './assets/img/person_1.png'
+  import person_2 from './assets/img/person_2.png'
+  import person_3 from './assets/img/person_3.png'
+
   const intros = reactive([
     {intro_url: rockstar_games_logo},
     {intro_url: rockstar_north_logo},
     {intro_url: gta_4_logo},
   ])
-  const is_showing = ref(-1)
+
+  const theme_items = reactive([
+    {bg: bg_1, person: person_1},
+    {bg: bg_2, person: person_2},
+    {bg: bg_3, person: person_3},
+  ])
+
+  const is_showing_intro = ref(-1)
+  const is_showing_theme = ref(-1)
+  const audio_player = ref('')
+  // audio_player.value.play()
   onMounted(() => {
-    is_showing.value++;
+    is_showing_intro.value++;
   })
   setInterval(() => {
-    // is_showing.value != 2 ? is_showing.value++ : null
-    is_showing.value++
-  },2400)
+    // is_showing_intro.value <= 2 ? is_showing_intro.value++ : null
+    if(is_showing_intro.value <= 2) {
+      is_showing_intro.value++
+      if(is_showing_intro.value == 2) {
+        setTimeout(() => {
+          is_showing_theme.value = 0
+        },3500)
+      }
+      return 
+    }
+  },3000)
+  setTimeout(() => {
+    setInterval(() => {
+      is_showing_theme.value++
+    },7000)
+  },9000)  
 </script>
 <template>
   <div class="container">
     <div v-for="(intro, index) in intros"  :key="index">
-        <img :src="intro.intro_url" v-if="is_showing == index" :class="'intro intro_logo_' + index">
+        <img :src="intro.intro_url" v-if="is_showing_intro == index" :class="'intro intro_logo_' + index">
     </div>
-    <button @click="is_showing++">
-      <h2>TIKLA</h2>
-    </button>
+    <!-- <audio :src="loading_audio_url" ref="audio_player"></audio> -->
+    <div v-for="(item, index) in theme_items" :key="index">
+      <img :src="item.bg" v-if="is_showing_theme == index" class="bg">
+    </div>
   </div>  
 </template>
 
@@ -37,6 +70,7 @@
   }
   .container {
     position: relative;
+    overflow: hidden;
     width: 100vw;
     height: 100vh;
     background: #000000;  // cotainer'in bg'sini degistirmene gerek yok zaten tanıtımlardan sonra sadece bg_image'ler gelip onu kaplayacak
@@ -83,6 +117,32 @@
           opacity: 0;
         }
       }
+    }
+    .bg {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      -webkit-filter: grayscale(100%); /* Safari 6.0 - 9.0 */
+      filter: grayscale(100%);
+      animation: theme_bg_animation 7s linear;
+      @keyframes theme_bg_animation {
+        0% {
+          transform: scale(1.20);
+          opacity: 0;
+        }
+        30% {
+          opacity: 1;
+        }
+        70% {
+          opacity: 1;
+        }
+        100% {
+          transform: scale(1);
+          opacity: 0;
+        }
+      }
+    }
   }
-}
 </style>
